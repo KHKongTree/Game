f03968eec2312131f3f38eeed78841cbbb45ff12
chanheeK0156 <cksgml0156@naver.com>
2023-03-02 16:38:32 +0900

    정리

--- game/src/game/dto/Item.java
+++ game/src/game/dto/Item.java
@@ -2,4 +2,107 @@ package game.dto;
 
 public class Item {
 
+	private int bronzeExeStriking = 10; // 동도끼 공격력
+	private int silverExeStriking = 20; // 은도끼 공격력
+	private int goldExeStrinking = 30;  // 금도끼 공격력
+	
+	private int bronzeExePrice = 100;   // 동도끼 가격
+	private int silverExePrice = 200;   // 은도끼 가격
+	private int goldExePrice = 300;     // 금도끼 가격
+	
+	private int fertiliserPrice = 50;   // 비료   가격
+	private int potionPrice = 100;      // 물약   가격
+	private int bugKillerPrice = 200;   // 해충제 가격
+	private int scissorPrice = 10;      // 가위   가격
+	
+	private int fertiliserCount = 0;    // 비료   개수
+	private int potionCount=0;          // 포션   개수
+	private int bugKillerCount=0;       // 해충제 개수
+	private int scissorCount=0;         // 가위   개수
+	
+
+	public int getBronzeExeStriking() {
+		return bronzeExeStriking;
+	}
+	public void setBronzeExeStriking(int bronzeExeStriking) {
+		this.bronzeExeStriking = bronzeExeStriking;
+	}
+	public int getSilverExeStriking() {
+		return silverExeStriking;
+	}
+	public void setSilverExeStriking(int silverExeStriking) {
+		this.silverExeStriking = silverExeStriking;
+	}
+	public int getGoldExeStrinking() {
+		return goldExeStrinking;
+	}
+	public void setGoldExeStrinking(int goldExeStrinking) {
+		this.goldExeStrinking = goldExeStrinking;
+	}
+	public int getBronzeExePrice() {
+		return bronzeExePrice;
+	}
+	public void setBronzeExePrice(int bronzeExePrice) {
+		this.bronzeExePrice = bronzeExePrice;
+	}
+	public int getSilverExePrice() {
+		return silverExePrice;
+	}
+	public void setSilverExePrice(int silverExePrice) {
+		this.silverExePrice = silverExePrice;
+	}
+	public int getGoldExePrice() {
+		return goldExePrice;
+	}
+	public void setGoldExePrice(int goldExePrice) {
+		this.goldExePrice = goldExePrice;
+	}
+	public int getFertiliserPrice() {
+		return fertiliserPrice;
+	}
+	public void setFertiliserPrice(int fertiliserPrice) {
+		this.fertiliserPrice = fertiliserPrice;
+	}
+	public int getPotionPrice() {
+		return potionPrice;
+	}
+	public void setPotionPrice(int potionPrice) {
+		this.potionPrice = potionPrice;
+	}
+	public int getBugKillerPrice() {
+		return bugKillerPrice;
+	}
+	public void setBugKillerPrice(int bugKillerPrice) {
+		this.bugKillerPrice = bugKillerPrice;
+	}
+	public int getScissorPrice() {
+		return scissorPrice;
+	}
+	public void setScissorPrice(int scissorPrice) {
+		this.scissorPrice = scissorPrice;
+	}
+	public int getFertiliserCount() {
+		return fertiliserCount;
+	}
+	public void setFertiliserCount(int fertiliserCount) {
+		this.fertiliserCount = fertiliserCount;
+	}
+	public int getPotionCount() {
+		return potionCount;
+	}
+	public void setPotionCount(int potionCount) {
+		this.potionCount = potionCount;
+	}
+	public int getBugKillerCount() {
+		return bugKillerCount;
+	}
+	public void setBugKillerCount(int bugKillerCount) {
+		this.bugKillerCount = bugKillerCount;
+	}
+	public int getScissorCount() {
+		return scissorCount;
+	}
+	public void setScissorCount(int scissorCount) {
+		this.scissorCount = scissorCount;
+	}
 }
