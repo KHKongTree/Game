61fbe2aac12a066fe31ff1a36a8bf6a7a5cf96b9
banghs <whoissyu@naver.com>
2023-03-02 16:07:31 +0900

    2023-03-02 DTO 구현

--- game/src/game/dto/Tree.java
+++ game/src/game/dto/Tree.java
@@ -1,5 +1,25 @@
 package game.dto;
 
 public class Tree {
-
+	
+	private String treeName; // 이름
+	private double treeHeight; // 길이
+	private double growthrate; // 성장 비율
+	
+	public String getTreeName() {
+		return treeName;
+	}
+	
+	public void setTreeName(String treeName) {
+		this.treeName = treeName;
+	}
+	
+	public double getTreeHeight() {
+		return treeHeight;
+	}
+	
+	public void setTreeHeight(double treeHeight) {
+		this.treeHeight = treeHeight;
+	}
+	
 }
--- game/src/game/dto/Woodcutter.java
+++ game/src/game/dto/Woodcutter.java
@@ -1,5 +1,15 @@
 package game.dto;
 
 public class Woodcutter {
-
+	
+	private int hp = 100; // 체력
+	
+	public int getHp() {
+		return hp;
+	}
+	
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	
 }
