52ffa2cc5c65d66cf64448283cca23bae465122f
banghs <whoissyu@naver.com>
2023-03-03 15:36:16 +0900

    20230303 상점 기능 구현

--- game/src/game/service/GameService.java
+++ game/src/game/service/GameService.java
@@ -1,13 +1,14 @@
 package game.service;
 
 import game.dto.Hero;
+import game.dto.Item;
 import game.view.GameView;
 
 public class GameService {
 	public static Hero signUp(String userId, String userPw, String userPwConfirm) {
 
 		Hero user = null;
-
+		
 		if (userPw.equals(userPwConfirm)) {
 
 			user = new Hero(userId, userPw);
@@ -29,5 +30,34 @@ public class GameService {
 		}
 	}
 
+	public boolean isLoginCheck() {
+		
+		return false;
+	}
+
+	public boolean buyWeapon(String weapon, int price, int exStrike) {
+		
+		if (GameView.loginUser.getGold() >= price) {
+			GameView.loginUser.setWeapon(weapon);
+			GameView.loginUser.setGold(GameView.loginUser.getGold()-price);
+			
+			GameView.loginUser.setStrike(exStrike);
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
+	
+	public boolean buyItem(int price) {
+		
+		if (GameView.loginUser.getGold() >= price) {
+			GameView.loginUser.setGold(GameView.loginUser.getGold()-price);
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
 
 }
