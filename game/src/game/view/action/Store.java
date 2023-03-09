65a5069a6205060eb9242c1d0c53acc18096e995
Kim Ji-hwan <lutunger@gmail.com>
2023-03-09 20:54:23 +0900

    에러 해결

--- game/src/game/view/action/Store.java
+++ game/src/game/view/action/Store.java
@@ -7,56 +7,53 @@ import game.image.GameUI;
 import game.service.UserItemService;
 
 public class Store {
-	
-	
-	
+
 	public void menu() {
 
-		try (Scanner sc = new Scanner(System.in)) {
-			Item item = new Item();
-			
-			UserItemService userItem = new UserItemService();
-			
-			int input = 0;
-			do {
-				
-				GameUI.store();
-				input = sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		Item item = new Item();
 
-				switch (input) {
-				case 1:
-					UserItemService.successBuyWeapon(item.getBronzeExe(), item.getBronzeExePrice(), item.getBronzeExeStriking());
-					break;
-				case 2:
-					UserItemService.successBuyWeapon(item.getSilverExe(), item.getSilverExePrice(), item.getSilverExeStriking());
-					break;
-				case 3:
-					UserItemService.successBuyWeapon(item.getGoldExe(), item.getGoldExePrice(), item.getGoldExeStrinking());
-					break;
-				case 4:
-					Item.setFertiliserCount(
-							Item.getFertiliserCount() + userItem.successBuyItem(item.getFertiliser(), item.getFertiliserPrice()));
-					break;
-				case 5:
-					Item.setPotionCount(Item.getPotionCount() + userItem.successBuyItem(item.getPotion(), item.getPotionPrice()));
-					break;
-				case 6:
-					Item.setBugKillerCount(
-							Item.getBugKillerCount() + userItem.successBuyItem(item.getBugKiller(), item.getPotionPrice()));
-					break;
-				case 7:
-					Item.setScissorCount(
-							Item.getScissorCount() + userItem.successBuyItem(item.getScissor(), item.getScissorPrice()));
-					break;
-				case 0:
-					break;
-				}
+		UserItemService userItem = new UserItemService();
+		int input = 0;
+		do {
 
-			} while (input != 0);
-		}
+			GameUI.store();
+			input = sc.nextInt();
+
+			switch (input) {
+			case 1:
+				UserItemService.successBuyWeapon(item.getBronzeExe(), item.getBronzeExePrice(),
+						item.getBronzeExeStriking());
+				break;
+			case 2:
+				UserItemService.successBuyWeapon(item.getSilverExe(), item.getSilverExePrice(),
+						item.getSilverExeStriking());
+				break;
+			case 3:
+				UserItemService.successBuyWeapon(item.getGoldExe(), item.getGoldExePrice(), item.getGoldExeStrinking());
+				break;
+			case 4:
+				Item.setFertiliserCount(Item.getFertiliserCount()
+						+ userItem.successBuyItem(item.getFertiliser(), item.getFertiliserPrice()));
+				break;
+			case 5:
+				Item.setPotionCount(
+						Item.getPotionCount() + userItem.successBuyItem(item.getPotion(), item.getPotionPrice()));
+				break;
+			case 6:
+				Item.setBugKillerCount(
+						Item.getBugKillerCount() + userItem.successBuyItem(item.getBugKiller(), item.getPotionPrice()));
+				break;
+			case 7:
+				Item.setScissorCount(
+						Item.getScissorCount() + userItem.successBuyItem(item.getScissor(), item.getScissorPrice()));
+				break;
+			case 0:
+				break;
+			}
+
+		} while (input != 0);
 		UserItemService.str = "";
 	}
-
-	
 
 }
